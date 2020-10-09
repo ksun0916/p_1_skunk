2e67d7f5972357a94f17de1a067c1860a9361ee2
ksun0916 <sunx0229@umn.edu>
2020-10-09 16:51:13 -0500

    add test_die_with_predictable_die

--- SkunkProject/test/skunk/domain/TestDie.java
+++ SkunkProject/test/skunk/domain/TestDie.java
@@ -1,14 +1,15 @@
 package skunk.domain;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
-
+import java.util.Random;
 import org.junit.Test;
 
 public class TestDie
 {
 	@Test
-	public void test_die_with_seed()
+	public void test_die_with_predictable_die()
 	{
-		fail();
+		fail();	
 	}
 	
 	@Test
