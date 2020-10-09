e53382312252441d04810291fab49bcc7265b986
ksun0916 <sunx0229@umn.edu>
2020-10-09 16:24:41 -0500

    initial TestDie and TestDice

--- SkunkProject/test/skunk/domain/TestDice.java
+++ SkunkProject/test/skunk/domain/TestDice.java
@@ -3,17 +3,11 @@ import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
-public class TestDie
+public class TestDice
 {
 	@Test
 	public void test()
 	{
 		fail("Not yet implemented");
 	}
-
-	//@Test(expected=RuntimeException.class)
-	//public void test2()
-	//{
-	//	throw new RuntimeException();
-	//}
 }
