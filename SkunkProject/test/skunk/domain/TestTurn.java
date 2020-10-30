08ef61bfd4218ec48f0abcfafdc7dda7625b18fd
ksun0916 <sunx0229@umn.edu>
2020-10-30 14:27:32 -0500

    add TestTurn

---
+++ SkunkProject/test/skunk/domain/TestTurn.java
@@ -0,0 +1,14 @@
+package skunk.domain;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestTurn {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
