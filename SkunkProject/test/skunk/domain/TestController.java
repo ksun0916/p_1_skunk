ca83a84ebd7ea00919031a4aafc1cbab8aed88d6
ksun0916 <sunx0229@umn.edu>
2020-10-30 14:48:13 -0500

    add TestController

---
+++ SkunkProject/test/skunk/domain/TestController.java
@@ -0,0 +1,14 @@
+package skunk.domain;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestController {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
